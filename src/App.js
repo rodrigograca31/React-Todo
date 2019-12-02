8f0368b9977c8866b7e921bef6ca2b15e68dd317
rodrigograca31 <rodrigograca1993@gmail.com>
2019-12-02 15:16:16 +0000

    Some styles

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import TodoList from "./components/TodoComponents/TodoList";
 import TodoForm from "./components/TodoComponents/TodoForm";
 import "./components/TodoComponents/Todo.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 class App extends React.Component {
 	// you will need a place to store your state in this component.
@@ -90,7 +91,7 @@ class App extends React.Component {
 
 	render() {
 		return (
-			<div>
+			<div className="app">
 				<h2>Welcome to your Todo App!</h2>
 				<TodoList todos={this.state.todos} complete={this.complete} />
 				<TodoForm addtodo={this.addtodo} clear={this.clear} />
--- src/components/TodoComponents/TodoForm.js
+++ src/components/TodoComponents/TodoForm.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Button, Input } from "reactstrap";
 
 class TodoForm extends React.Component {
 	constructor(props) {
@@ -32,27 +33,52 @@ class TodoForm extends React.Component {
 	render() {
 		return (
 			<form action="#" onSubmit={this.onFormSubmit}>
-				<input
+				{/* <input
 					type="text"
 					value={this.state.formValues.task}
 					onChange={this.onValueChange}
 					name="task"
-				/>
-				<input
+				/> */}
+				<Input
+					type="text"
+					value={this.state.formValues.task}
+					onChange={this.onValueChange}
+					name="task"
+				></Input>
+				{/* <input
 					type="button"
 					value="Add Todo"
 					onClick={e => {
 						this.setState({ formValues: { task: "" } });
 						this.props.addtodo(this.state.formValues.task);
 					}}
-				/>
-				<input
+				/> */}
+				<br />
+				<br />
+				<Button
+					color="success"
+					onClick={e => {
+						this.setState({ formValues: { task: "" } });
+						this.props.addtodo(this.state.formValues.task);
+					}}
+				>
+					Add Todo
+				</Button>{" "}
+				<Button
+					color="success"
+					onClick={e => {
+						this.props.clear();
+					}}
+				>
+					Clear Completed
+				</Button>
+				{/* <input
 					type="button"
 					value="Clear Completed"
 					onClick={e => {
 						this.props.clear();
 					}}
-				/>
+				/> */}
 			</form>
 		);
 	}
